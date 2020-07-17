da53032422934150721b9b651d2e4aa76cacbf45
Kyle Poe <kyle@kpoe.me>
2020-07-17 09:27:31 -0700

    Test commit

---
+++ Code/Tests/data_examine.py
@@ -0,0 +1,23 @@
+#!/usr/env/bin python
+
+# From https://github.com/NeuromatchAcademy/course-content/blob/master/projects/load_stringer_spontaneous.ipynb
+import os, requests
+import numpy as np
+
+fname = "stringer_spontaneous.npy"
+url = "https://osf.io/dpqaj/download"
+
+if not os.path.isfile(fname):
+  try:
+    r = requests.get(url)
+  except requests.ConnectionError:
+    print("!!! Failed to download data !!!")
+  else:
+    if r.status_code != requests.codes.ok:
+      print("!!! Failed to download data !!!")
+    else:
+      with open(fname, "wb") as fid:
+        fid.write(r.content)
+
+dat = np.load('stringer_spontaneous.npy', allow_pickle=True).item()
+print(dat.keys())
