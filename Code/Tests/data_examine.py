f393e3ad0a18fa486ef318c1d98e7a807ae0076f
Kyle Poe <kyle@kpoe.me>
2020-07-17 15:29:12 -0700

    Made some trivial changes

--- Code/Tests/data_examine.py
+++ Code/Tests/data_examine.py
@@ -1,23 +1,16 @@
 #!/usr/env/bin python
 
 # From https://github.com/NeuromatchAcademy/course-content/blob/master/projects/load_stringer_spontaneous.ipynb
-import os, requests
 import numpy as np
 
-fname = "../../Data/stringer_spontaneous.npy"
-url = "https://osf.io/dpqaj/download"
+if __name__ == '__main__':
+    from download_data import download_data
+else:
+    from .download_data import download_data
 
-if not os.path.isfile(fname):
-  try:
-    r = requests.get(url)
-  except requests.ConnectionError:
-    print("!!! Failed to download data !!!")
-  else:
-    if r.status_code != requests.codes.ok:
-      print("!!! Failed to download data !!!")
-    else:
-      with open(fname, "wb") as fid:
-        fid.write(r.content)
 
-dat = np.load('stringer_spontaneous.npy', allow_pickle=True).item()
+download_data()
+
+fname = "Data/stringer_spontaneous.npy"
+dat = np.load(fname, allow_pickle=True).item()
 print(dat.keys())
--- Code/Tests/download_data.py
+++ Code/Tests/download_data.py
@@ -15,18 +15,22 @@ data = {
     }
 }
 
-for dataset in data.keys():
-    if not os.path.isfile(data[dataset]['file']):
-        try:
-            print(f'Downloading {str(dataset)} data...')
-            r = requests.get(data[dataset]['url'])
-        except requests.ConnectionError:
-            print("!!! Failed to download data !!!")
-
-        else:
-            if r.status_code != requests.codes.ok:
+def download_data():
+    for dataset in data.keys():
+        if not os.path.isfile(data[dataset]['file']):
+            try:
+                print(f'Downloading {str(dataset)} data...')
+                r = requests.get(data[dataset]['url'])
+            except requests.ConnectionError:
                 print("!!! Failed to download data !!!")
 
             else:
-                with open(data[dataset]['file'], "wb") as fid: 
-                    fid.write(r.content)
+                if r.status_code != requests.codes.ok:
+                    print("!!! Failed to download data !!!")
+
+                else:
+                    with open(data[dataset]['file'], "wb") as fid: 
+                        fid.write(r.content)
+
+if __name__ == '__main__':
+    download_data()
