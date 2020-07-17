9cc04a3ad5ae43ef428afba939c386ba2191cbea
Kyle Poe <kyle@kpoe.me>
2020-07-17 09:59:31 -0700

    Added a download script

---
+++ Code/Tests/download_data.py
@@ -0,0 +1,32 @@
+#!/usr/env/bin python
+
+# From https://github.com/NeuromatchAcademy/course-content/blob/master/projects/load_stringer_spontaneous.ipynb
+import os, requests
+import numpy as np
+
+data = {
+    'Orientation': {
+        'url': "https://osf.io/ny4ut/download",
+        'file': "stringer_orientations.npy"
+    },
+    'Spontaneous': {
+        'url': "https://osf.io/dpqaj/download",
+        'file': "stringer_spontaneous.npy"
+    }
+}
+
+for dataset in data.keys():
+    if not os.path.isfile(data[dataset]['file']):
+        try:
+            print(f'Downloading {str(dataset)} data...')
+            r = requests.get(data[dataset]['url'])
+        except requests.ConnectionError:
+            print("!!! Failed to download data !!!")
+
+    else:
+        if r.status_code != requests.codes.ok:
+            print("!!! Failed to download data !!!")
+
+        else:
+            with open(data[dataset]['file'], "wb") as fid:
+                fid.write(r.content)
