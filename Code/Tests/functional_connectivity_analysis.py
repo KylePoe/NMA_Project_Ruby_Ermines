6be026c53ad24bebcf7e7226b70246d8a0cee880
Kyle Poe <kyle@kpoe.me>
2020-07-23 19:55:40 -0700

    Thursday update

--- Code/Tests/functional_connectivity_analysis.py
+++ Code/Tests/functional_connectivity_analysis.py
@@ -17,12 +17,23 @@ ordered_linkage = optimal_leaf_ordering(linkage_matrix, pearson_correlation)
 index = leaves_list(ordered_linkage)
 connectivity_matrix = pearson_correlation[index, :][:, index]
 
+# Look at euclidean distance
+neuron_locs = data['xyz']
+dist_mat = np.zeros([n_neurons, n_neurons])
+
+for i in range(n_neurons):
+    for j in range(n_neurons):
+        dist_mat[i, j] = np.linalg.norm(neuron_locs[:, i] - neuron_locs[:, j])
+
 # Save to file
 np.save(
     f'Data/connectivity_analysis_spontaneous.npy', {
         'Connectivity Matrix': connectivity_matrix,
         'Index': index,
         'Ordered Linkage': ordered_linkage,
-        'Linkage Matrix': linkage_matrix
+        'Linkage Matrix': linkage_matrix,
+        'Distance Matrix': dist_mat
     }
 )
+
+# For an example output,
